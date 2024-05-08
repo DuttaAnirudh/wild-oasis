ffd8b90d94b1c2c739a7a79d2e5fcb6491a55d3b
DuttaAnirudh <worktodutta@gmail.com>
2024-05-08 18:58:26 +0530

    chore: connect supabase db to the app; set up tanstack react query

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabaseUrl = "https://khqyahuhuknuctbwujqy.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtocXlhaHVodWtudWN0Ynd1anF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUwOTQ4NzQsImV4cCI6MjAzMDY3MDg3NH0.9Lmz_dtJBruYA6ntIqqjW1opgsJcLermxH0IAPYfY6I";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
