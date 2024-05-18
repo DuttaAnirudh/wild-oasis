5de0ff1fb6312be48f66a7bbc569f56952007627
DuttaAnirudh <worktodutta@gmail.com>
2024-05-18 18:26:14 +0530

    feat: authenticate user login with supabase

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export const useLogin = () => {
+  const navigate = useNavigate();
+
+  const { mutate: login, isPending: isLogingIn } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.error("Error: ", err);
+      toast.error("Provided email or password in incorrect");
+    },
+  });
+
+  return { login, isLogingIn };
+};
